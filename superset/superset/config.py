1e63c7e12146c4349fe3d0fd8d2457ba4bf9d0bc
bakasajoshua <baksajoshua09@gmail.com>
2026-06-23 16:00:53 +0300

    Ignore docker .env

--- superset/superset/config.py
+++ superset/superset/config.py
@@ -934,7 +934,7 @@ BACKUP_COUNT = 30
 QUERY_LOGGER = None
 
 # Set this API key to enable Mapbox visualizations
-# MAPBOX_API_KEY = os.environ.get("MAPBOX_API_KEY", "")
+MAPBOX_API_KEY = os.environ.get("MAPBOX_API_KEY", "")
 
 # Maximum number of rows returned for any analytical database query
 SQL_MAX_ROW = 100000
